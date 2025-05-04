0a329570bcba1489ae4ae7e51b9f183e145b01a2
Gianluca Zani <gianlucazani@MacBook-Pro-di-Gianluca.local>
2025-05-04 13:33:41 +0200

    UPDATE: differentiated endpoints for all KPIs or just PA

--- src/pollinator_abundance/server.py
+++ src/pollinator_abundance/server.py
@@ -3,8 +3,17 @@ from .handler import pollinator_abundance_calculation
 
 app = FastAPI()
 
-
 @app.get("/calculate")
+async def calculate():
+    all_kpis = pollinator_abundance_calculation()
+    result = {
+        "CA": all_kpis["result_values"]["CA"]["PA"],
+        "ROI": all_kpis["result_values"]["ROI"]["PA"],
+        "Delta": all_kpis["result_values"]["Delta"]["PA"],
+    }
+    return result
+
+@app.get("/calculate_all_kpis")
 async def calculate():
     result = pollinator_abundance_calculation()
     return result["result_values"]
